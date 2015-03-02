b6f7d27d17b8acfaebe33df394675b65447504a5
lavedream <lavedream@hotmail.com>
2015-03-02 20:17:27 +0800

    修改配置信息，dao 注入没有成功，basepackage 的包路径 没有 写正确

--- supermarket/src/main/java/com/flyjaky/supermarket/controller/HelloController.java
+++ supermarket/src/main/java/com/flyjaky/supermarket/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.flyjaky.supermarket.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.flyjaky.supermarket.service.ProductService;
+
 @Controller
 public class HelloController {
+	
+	@Resource
+	private ProductService productService; 
 
 	@RequestMapping
 	public void hello(){
+		List<Map<String,Object>> data=productService.getProductAll();
+		System.out.println(data.size());
 		System.out.println("hello");
 	}
 }
--- supermarket/src/main/java/com/flyjaky/supermarket/dao/ProductDao.java
+++ supermarket/src/main/java/com/flyjaky/supermarket/dao/ProductDao.java
@@ -3,9 +3,20 @@ package com.flyjaky.supermarket.dao;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author lavedream
+ * @date 2015/03/02 16:47
+ * @desc 商品操作类  project operation class
+// * 
+// * */
 public interface ProductDao {
 	
 	
-	public List<Map<String,Object>> getUserAll();
+	/**
+	 * @author lavedream
+	 * @date 2015/03/02 16:47
+	 * @desc 查询所有商品信息
+	 * */
+	public List<Map<String,Object>> getProductAll();
 	
 }
